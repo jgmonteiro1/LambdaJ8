680fed560e3b2964a6e0776a2530b5f2a9a116c2
jgmonteiro <joao.monteiro@bepay.me>
2020-12-15 12:00:11 -0300

    3 modos de fazer sem utilizar Lambda

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
+import consumer.Mostrador;
 import entities.Usuario;
 
 public class Main {
@@ -16,6 +18,14 @@ public class Main {
 		System.out.println(u.getNome());
 		}
 		
+		Mostrador mostrador = new Mostrador();
+		usuarios.forEach(mostrador);
+		
+		usuarios.forEach(new Consumer<Usuario>() {
+			public void accept(Usuario u) {
+				System.out.println(u.getNome());
+			}
+		});
 	}
 
 }
