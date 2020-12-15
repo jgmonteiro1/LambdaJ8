d99e380e45e20440f7b9d2aea04196667f11fd93
jgmonteiro <joao.monteiro@bepay.me>
2020-12-15 14:24:17 -0300

    Utilizando lambda de dois modos

--- src/Main.java
+++ src/Main.java
@@ -13,19 +13,30 @@ public class Main {
 		Usuario user2 = new Usuario("Rodrigo Turini", 120);
 		Usuario user3 = new Usuario("Guilherme Silveira", 190);
 		
+
 		List<Usuario> usuarios = Arrays.asList(user1, user2, user3);
+		
+		//Percorrendo a lista
 		for(Usuario u : usuarios) {
 		System.out.println(u.getNome());
 		}
+		System.out.println("----------------");
 		
+		//Percorrendo a lista utilizando um consumer
 		Mostrador mostrador = new Mostrador();
 		usuarios.forEach(mostrador);
 		
+		System.out.println("----------------");
+		//Percorrendo a lista utilizando una função anônima
 		usuarios.forEach(new Consumer<Usuario>() {
 			public void accept(Usuario u) {
 				System.out.println(u.getNome());
 			}
 		});
+		
+		Consumer<Usuario> lambda =  u -> System.out.println(u.getNome());
+		System.out.println("----------------");
+		usuarios.forEach(u -> System.out.println(u.getNome()));
 	}
 
 }
